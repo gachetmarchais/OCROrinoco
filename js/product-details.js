375c4ccfb17ec0fc0331c54b74490ce0a61c25a1
Natacha <natacha.marchais84@gmail.com>
2020-09-15 11:54:10 +0200

    150920

--- js/product-details.js
+++ js/product-details.js
@@ -1,4 +1,74 @@
+//récupérer l'Id du teddy
 const url = new URL(window.location.href);
 const teddy_id = url.searchParams.get('id');
-
+console.log(teddy_id); //à retirer
 // lien explicatif avec exemple : https://developers.google.com/web/updates/2016/01/urlsearchparams
+
+
+// se connecter aux teddies du server via l'url
+async function ajaxGet(){
+    const url = 'http://localhost:3000/api/teddies/' + teddy_id;
+    let response = await fetch(url); 
+
+    if (response.ok) { 
+        let json = await response.json();
+        return json;
+    } else {
+    alert("HTTP-Error: " + response.status);
+    }
+};
+
+// récupérer des Id des balises html
+const image = document.getElementById('teddy-image');
+const name = document.getElementById('teddy-name');
+const description = document.getElementById('teddy-description');
+const price = document.getElementById('teddy-price');
+const selectColors = document.getElementById('teddy-color');
+const quantity = document.getElementById('teddy-quantity');
+const addBasket = document.getElementById('add-basket');
+
+// récupérer le teddy du server
+ajaxGet().then( teddy => {
+    console.log(teddy);
+
+    // remplir les champs html par les infos du teddy
+        image.innerHTML = `<img id="teddy-image" src="${teddy.imageUrl}" alt="Photo de ${teddy.name}" >`;
+        name.innerHTML = teddy.name;
+        description.innerHTML = teddy.description;
+        price.innerHTML = teddy.price;
+
+   // ajouter les différents choix de couleurs -- A FAIRE 
+        
+        ajaxGet().then(colors => {           
+            colors.forEach (color => {
+                $selectColors.innerHTML += `
+                '<option id="color-option">' + ${colors[i]} + '</option>';`
+            });
+            console.log(selectColors);          
+            /*
+           for (i = 0; i < colors.length; i++) {
+                $selectColors.innerHTML += 
+                '<option id="colors-options">' + colors[i] + '</option>';
+            }; */
+        });
+        
+
+    // ajouter les quantités -- A FAIRE 
+        
+
+
+    // activer le bouton d'ajout au panier -- PAS FINI
+        addBasket.addEventListener('click', function(event){
+            event.preventDefault();
+            let element = {};
+            element.id = teddy_id;
+            element.name = teddy.name;
+            element.price = teddy.price;
+            element.quantity = teddy.quantity;
+
+            let basket = [];
+
+        });
+   
+});
+
